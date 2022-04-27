cb47b2118bc8b58544063086b2341dc4c1d9c0d5
AshrakatAbdELSattar <aabdelsattar@informatique-eg.com>
2022-04-27 13:12:50 +0200

    First V

---
+++ Simple Project/src/simple/project/SimpleProject.java
@@ -0,0 +1,12 @@
+
+package simple.project;
+
+
+public class SimpleProject {
+
+    
+    public static void main(String[] args) {
+     System.out.println("Hello, World!");
+    }
+    
+}
---
+++ Simple Project/src/simple/project/student.java
@@ -0,0 +1,6 @@
+
+package simple.project;
+
+public class student {
+    
+}
